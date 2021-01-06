a253f894c400b0c0fb3ac1923e30a34641c6a254
Leonardo Fabían Hernández Peña <ingenieroleonardo@outlook.com>
2021-01-06 15:54:32 -0500

    Refactor de código

--- DatingApp/Startup.cs
+++ DatingApp/Startup.cs
@@ -20,12 +20,12 @@ namespace DatingApp
 {
     public class Startup
     {
+        private readonly IConfiguration Configuration;
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureDebelopmentServices(IServiceCollection services)
